9cd538ab9c224db002c85167ad600591c6f89020
Michael Bond <software@the-forgotten.org>
2019-04-17 14:49:18 -0400

    debugging

--- src/google-login.js
+++ src/google-login.js
@@ -35,6 +35,8 @@ class GoogleLogin extends Component {
       jsSrc
     } = this.props
 
+    console.log("SCOPE 1 -------------", scope);
+
     loadScript(document, 'script', 'google-login', jsSrc, () => {
       const params = {
         client_id: clientId,
@@ -95,6 +97,7 @@ class GoogleLogin extends Component {
       if (responseType === 'code') {
         auth2.grantOfflineAccess(options).then(res => onSuccess(res), err => onFailure(err))
       } else {
+        console.log("options ---------------", options);
         auth2.signIn(options).then(res => this.handleSigninSuccess(res), err => onFailure(err))
       }
     }
